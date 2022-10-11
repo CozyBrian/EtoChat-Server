f0c7a695787623389f03aa712424b45efb48459b
CozyBrian <dragonbrian33@gmail.com>
2022-10-11 11:03:39 +0000

    express setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const COber: string = "name";
+import express, { Request, Response } from "express";
 
-console.log(COber);
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello worldddd");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
