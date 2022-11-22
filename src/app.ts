093eaffbccc52e0b965506cc58e564e53a987617
CozyBrian <dragonbrian33@gmail.com>
2022-11-22 15:08:19 +0000

    self serve

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-const nanoid = require("nanoid");
+import path from "path";
 
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   })
-);
-
-app.get("/", (req: Request, res: Response) => {
-  const id = nanoid();
-  res.redirect(`/${id}`);
-});
-
+  );
+  
+  app.get("/", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+  app.use(express.static("public"));
+  
 export default app;
