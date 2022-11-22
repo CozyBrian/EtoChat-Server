9a3407254879259c050b7a33eb83cbda93050562
CozyBrian <dragonbrian33@gmail.com>
2022-11-22 15:28:44 +0000

    react serve test

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,19 @@ import path from "path";
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   })
-  );
+);
   
-  app.get("/", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-  });
-  app.use(express.static("public"));
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "public", "..", "index.html"));
+});
+
+app.use(express.static("public"));
   
 export default app;
